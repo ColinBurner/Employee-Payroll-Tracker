56f1f0a8bf6d5f1905e543e84262674236f137d4
Colin Taaffe <85810714+BurnerTV@users.noreply.github.com>
2024-05-13 15:16:02 -0500

    added display average salary to console log

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
-let employees = [];
+let employeesArray = [];
 
 // Collect employee data
 const collectEmployees = function() {
@@ -18,46 +18,59 @@ const collectEmployees = function() {
       break;
     }
 
-    let salary = prompt("Enter salary: ", "Salary");
+    let salaryInput = prompt("Enter salary: ", "Salary");
 
-    if (salary === null) {
+    if (salaryInput === null) {
       break;
     }
 
     let employee = {
       firstName: firstName,
       lastName: lastName,
-      salary: parseFloat(salary)
+      salary: salaryInput 
     };
 
-    employees.push(employee);
+    employeesArray.push(employee);
   
     let choice = prompt("OK to continue or press 'cancel' to display data");
 
     if (choice === null) {
-      return employees;
+      return employeesArray;
     }
   }
-  console.log(employees);
+  console.log(employeesArray);
 }
-
-
-  // TODO: Get user input to create and return an array of employee objects
-
+// TODO: Get user input to create and return an array of employee objects
 
 
 // Display the average salary
-const displayAverageSalary = function(employees) {
-  // TODO: Calculate and display the average salary
-  let totalSalary = 0;
-  for (let i = 0; i < employees.length; i++) {
-    totalSalary += employees[i].salary;
+const calculateAverageSalary = function(employeesArray) {
+  if (employeesArray.length === 0) {
+    return 0; // Return 0 if there are no employees
   }
-  let averageSalary = totalSalary / employees.length;
 
-  console.log("The average employee salary between our 3 employee(s) is: $" + averageSalary.toFixed(2));
+  let totalSalary = 0;
+
+  employeesArray.forEach(employee => {
+    // Parse the salary string into a number
+    let salary = parseFloat(employee.salary.replace(/[^\d.]/g, ''));
+
+    // Check if the parsed salary is a valid number
+    if (!isNaN(salary)) {
+      totalSalary += salary;
+    }
+  });
+
+  return totalSalary / employeesArray.length;
 }
 
+// Function to display average salary
+const displayAverageSalary = function(employeesArray) {
+  const averageSalary = calculateAverageSalary(employeesArray);
+  console.log("The average employee salary between our 3 employee(s) is $", averageSalary.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}));
+}
+
+  
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
   // TODO: Select and display a random employee
